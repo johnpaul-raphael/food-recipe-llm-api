2e057bf4655ed6cb7815e33b2ccdb06718ea8f47
Johnpaul Raphael <paul.jopal@gmail.com>
2026-05-10 00:40:01 -0400

    Dockerized the application

--- src/model/inference.py
+++ src/model/inference.py
@@ -5,8 +5,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 load_dotenv()
 
 MODEL_PATH = os.getenv("MODEL_PATH")
-MAX_NEW_TOKENS = os.getenv("MAX_NEW_TOKENS")
-TEMPERATURE = os.getenv("TEMPERATURE")
+MAX_NEW_TOKENS = int(os.getenv("MAX_NEW_TOKENS", 300))
+TEMPERATURE = float(os.getenv("TEMPERATURE", 0.7))
 
 
 class RecipeGenerator:
@@ -22,14 +22,10 @@ class RecipeGenerator:
         """
         Generate a recipe based on the given prompt.
         """
-        try:
-            formated_prompt = f"Recipe: {prompt}\nIngredients: \nInstructions: \n"
-            result = self.generator(formated_prompt, max_new_tokens=MAX_NEW_TOKENS, temperature=TEMPERATURE,
-            do_sample=True, top_p=0.9, pad_token_id=self.tokenizer.eos_token_id)
-            return result[0]['generated_text']
-        except Exception as e:
-            print(f"Error while generating recipe: {e}")
-            return None
+        formated_prompt = f"Recipe: {prompt}\nIngredients: \nInstructions: \n"
+        result = self.generator(formated_prompt, max_new_tokens=MAX_NEW_TOKENS, temperature=TEMPERATURE,
+        do_sample=True, top_p=0.9, pad_token_id=self.tokenizer.eos_token_id)
+        return result[0]['generated_text']
 
 if __name__ == "__main__":
     generator = RecipeGenerator()
